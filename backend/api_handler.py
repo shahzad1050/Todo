c8b694e6fbb643a31cb39f7df989b9fcac527433
shahzad1050 <shahzadarham266@gmail.com>
2026-01-16 03:43:44 +0500

    todo

--- backend/api_handler.py
+++ backend/api_handler.py
@@ -2,9 +2,4 @@ from main import app
 from mangum import Mangum
 
 # Create the Mangum handler to convert FastAPI app to Vercel serverless function
-mangum_handler = Mangum(app, lifespan="off")
-
-
-def handler(request, *args, **kwargs):
-    """Vercel serverless function handler"""
-    return mangum_handler(request, *args, **kwargs)
+handler = Mangum(app, lifespan="off")
